8b0f96e7b53e6d812e9ae1c70fa58c4f85b97639
Jaren-Wheeler <git@github.com>
2023-11-28 19:40:50 -0700

    Create 16 divs and append them to the window div

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let createElement = (element) => {
+    return document.createElement(element);
+}
+
+const window = document.querySelector(".window");
+
+let square1 = createElement("div");
+let square2 = createElement("div");
+let square3 = createElement("div");
+let square4 = createElement("div");
+let square5 = createElement("div");
+let square6 = createElement("div");
+let square7 = createElement("div");
+let square8 = createElement("div");
+let square9 = createElement("div");
+let square10 = createElement("div");
+let square11 = createElement("div");
+let square12 = createElement("div");
+let square13= createElement("div");
+let square14 = createElement("div");
+let square15 = createElement("div");
+let square16 = createElement("div");
+
+window.appendChild(square1);
+window.appendChild(square2);
+window.appendChild(square3);
+window.appendChild(square4);
+window.appendChild(square5);
+window.appendChild(square6);
+window.appendChild(square7);
+window.appendChild(square8);
+window.appendChild(square9);
+window.appendChild(square10);
+window.appendChild(square11);
+window.appendChild(square12);
+window.appendChild(square13);
+window.appendChild(square14);
+window.appendChild(square15);
+window.appendChild(square16);
