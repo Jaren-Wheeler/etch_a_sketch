ac415c56acb129fc26d3f63ebee6d0d9a49cdf53
Jaren-Wheeler <git@github.com>
2023-11-28 20:50:48 -0700

    Add mouseover events to grid so that when the mouse hovers over a specific square it lights up green.

--- script.js
+++ script.js
@@ -52,4 +52,21 @@ win.appendChild(square14);
 win.appendChild(square15);
 win.appendChild(square16);
 
+let square1Hover = square1.addEventListener("mouseover", () => square1.style.backgroundColor = "#00ff00");
+let square2Hover = square2.addEventListener("mouseover", () => square2.style.backgroundColor = "#00ff00");
+let square3Hover = square3.addEventListener("mouseover", () => square3.style.backgroundColor = "#00ff00");
+let square4Hover = square4.addEventListener("mouseover", () => square4.style.backgroundColor = "#00ff00");
+let square5Hover = square5.addEventListener("mouseover", () => square5.style.backgroundColor = "#00ff00");
+let square6Hover = square6.addEventListener("mouseover", () => square6.style.backgroundColor = "#00ff00");
+let square7Hover = square7.addEventListener("mouseover", () => square7.style.backgroundColor = "#00ff00");
+let square8Hover = square8.addEventListener("mouseover", () => square8.style.backgroundColor = "#00ff00");
+let square9Hover = square9.addEventListener("mouseover", () => square9.style.backgroundColor = "#00ff00");
+let square10Hover = square10.addEventListener("mouseover", () => square10.style.backgroundColor = "#00ff00");
+let square11Hover = square11.addEventListener("mouseover", () => square11.style.backgroundColor = "#00ff00");
+let square12Hover = square12.addEventListener("mouseover", () => square12.style.backgroundColor = "#00ff00");
+let square13Hover = square13.addEventListener("mouseover", () => square13.style.backgroundColor = "#00ff00");
+let square14Hover = square14.addEventListener("mouseover", () => square14.style.backgroundColor = "#00ff00");
+let square15Hover = square15.addEventListener("mouseover", () => square15.style.backgroundColor = "#00ff00");
+let square16Hover = square16.addEventListener("mouseover", () => square16.style.backgroundColor = "#00ff00");
+
 
