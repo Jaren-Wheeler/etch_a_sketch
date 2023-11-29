0003d703a8aced7301bd8cf48bdc519e0d60c264
Jaren-Wheeler <git@github.com>
2023-11-28 20:39:59 -0700

    Create grid. Create the elements in JS, adding them as children nodes to the window class. Then style the divs to fit as a square 4x4 grid on the page.

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
 
-let createElement = (element) => {
-    return document.createElement(element);
-}
+let win = document.querySelector(".window");
 
-const window = document.querySelector(".window");
+let square1 = document.createElement("div");
+let square2 = document.createElement("div");
+let square3 = document.createElement("div");
+let square4 = document.createElement("div");
+let square5 = document.createElement("div");
+let square6 = document.createElement("div");
+let square7 = document.createElement("div");
+let square8 = document.createElement("div");
+let square9 = document.createElement("div");
+let square10 = document.createElement("div");
+let square11 = document.createElement("div");
+let square12 = document.createElement("div");
+let square13 = document.createElement("div");
+let square14 = document.createElement("div");
+let square15 = document.createElement("div");
+let square16 = document.createElement("div");
 
-let square1 = createElement("div");
-let square2 = createElement("div");
-let square3 = createElement("div");
-let square4 = createElement("div");
-let square5 = createElement("div");
-let square6 = createElement("div");
-let square7 = createElement("div");
-let square8 = createElement("div");
-let square9 = createElement("div");
-let square10 = createElement("div");
-let square11 = createElement("div");
-let square12 = createElement("div");
-let square13= createElement("div");
-let square14 = createElement("div");
-let square15 = createElement("div");
-let square16 = createElement("div");
+square1.setAttribute("class", "square1");
+square2.setAttribute("class", "square2");
+square3.setAttribute("class", "square3");
+square4.setAttribute("class", "square4");
+square5.setAttribute("class", "square5");
+square6.setAttribute("class", "square6");
+square7.setAttribute("class", "square7");
+square8.setAttribute("class", "square8");
+square9.setAttribute("class", "square9");
+square10.setAttribute("class", "square10");
+square11.setAttribute("class", "square11");
+square12.setAttribute("class", "square12");
+square13.setAttribute("class", "square13");
+square14.setAttribute("class", "square14");
+square15.setAttribute("class", "square15");
+square16.setAttribute("class", "square16");
 
-window.appendChild(square1);
-window.appendChild(square2);
-window.appendChild(square3);
-window.appendChild(square4);
-window.appendChild(square5);
-window.appendChild(square6);
-window.appendChild(square7);
-window.appendChild(square8);
-window.appendChild(square9);
-window.appendChild(square10);
-window.appendChild(square11);
-window.appendChild(square12);
-window.appendChild(square13);
-window.appendChild(square14);
-window.appendChild(square15);
-window.appendChild(square16);
+win.appendChild(square1);
+win.appendChild(square2);
+win.appendChild(square3);
+win.appendChild(square4);
+win.appendChild(square5);
+win.appendChild(square6);
+win.appendChild(square7);
+win.appendChild(square8);
+win.appendChild(square9);
+win.appendChild(square10);
+win.appendChild(square11);
+win.appendChild(square12);
+win.appendChild(square13);
+win.appendChild(square14);
+win.appendChild(square15);
+win.appendChild(square16);
+
+
